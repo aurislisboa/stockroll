def556ae7c99a8d99b06ac2b26d655cdd231d6c6
Auris <aurislisboa@gmail.com>
2024-05-09 06:55:03 -0300

    finalizado LocalController

--- src/main/java/br/com/usystem/stockroll/controller/LocalController.java
+++ src/main/java/br/com/usystem/stockroll/controller/LocalController.java
@@ -2,24 +2,75 @@ package br.com.usystem.stockroll.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.usystem.stockroll.models.Local;
 import br.com.usystem.stockroll.repositories.LocalRepository;
 
 @Controller
-@RequestMapping("local")
+@RequestMapping("/local")
 public class LocalController {
     
     @Autowired
     private LocalRepository localRepository;
 
-
+    @GetMapping
     public ModelAndView listar() {
         var modelAndView = new ModelAndView("local/listar");
 
         modelAndView.addObject("locais", localRepository.findAll());
         return modelAndView;
     }
+
+    @GetMapping("/{id}")
+    public ModelAndView detalhar(@PathVariable Integer id) {
+        var modelAndView = new ModelAndView("local/detalhar");
+
+        modelAndView.addObject("local", localRepository.getReferenceById(id));
+        return modelAndView;
+    }
+
+
+    @GetMapping("/cadastrar") 
+    public ModelAndView cadastrar() {
+        var modelAndView = new ModelAndView("local/formulario");
+
+        modelAndView.addObject("local", new Local());
+        return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Local local) {
+        localRepository.save(local);
+
+        return "redirect:/local";
+    }
+
+    @GetMapping("/editar/{id}")
+    public ModelAndView editar(@PathVariable Integer id) {
+        var modelAndView = new ModelAndView("local/formulario");
+        
+        modelAndView.addObject("local", localRepository.getReferenceById(id));
+        return modelAndView;
+    }
+
+    @PostMapping("/editar/{id}")
+    public String editar(Local local) {
+        localRepository.save(local);
+
+        return "redirect:/local";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable Integer id) {
+        localRepository.deleteById(id);
+        
+        return "redirect:/local";
+    }
+
 
 }
