876b0496addd904370a5878b27498598dc2c8619
Auris <aurislisboa@gmail.com>
2024-06-16 05:10:00 -0300

    atualização no gráfico de pizza

--- src/main/java/br/com/usystem/stockroll/controller/DashboardController.java
+++ src/main/java/br/com/usystem/stockroll/controller/DashboardController.java
@@ -1,6 +1,8 @@
 package br.com.usystem.stockroll.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,21 +25,31 @@ public class DashboardController {
    
     @GetMapping
     public ModelAndView dashboard() {
-		var modelAndView = new ModelAndView("/dashboard");
+        var modelAndView = new ModelAndView("/dashboard");
 
-		Integer totalEstoque = loteRepository.selectSumQtdProdutos();
-    BigDecimal valorEstoque = loteRepository.selectSumValorUnitario();
+        Integer totalEstoque = loteRepository.selectSumQtdProdutos();
+        BigDecimal valorEstoque = loteRepository.selectSumValorUnitario();
 
-        modelAndView.addObject("totalEstoque", totalEstoque);
-        modelAndView.addObject("valorEstoque", valorEstoque);
-        modelAndView.addObject("quiosques", dashboardService.resumoDosQuiosques());
+          modelAndView.addObject("totalEstoque", totalEstoque);
+          modelAndView.addObject("valorEstoque", valorEstoque);
+          modelAndView.addObject("quiosques", dashboardService.resumoDosQuiosques());
 
-        /* pesquisa o nível do estoque e emite um alerta */
-        // modelAndView.addObject("alertas", estoqueService.verficaStatusDoEstoque());
-        // modelAndView.addObject("alertas", "Leite em Pó - 1Kg");
-        
-        modelAndView.addObject("alertas", dashboardService.alertaMinimoEmEstoque());
-        System.out.println(dashboardService.alertaMinimoEmEstoque()); 
+          /* pesquisa o nível do estoque e emite um alerta */
+          // modelAndView.addObject("alertas", estoqueService.verficaStatusDoEstoque());
+          // modelAndView.addObject("alertas", "Leite em Pó - 1Kg");
+          
+          modelAndView.addObject("alertas", dashboardService.alertaMinimoEmEstoque());
+          // System.out.println(dashboardService.alertaMinimoEmEstoque()); 
+
+          Map<String, Integer> surveyMap = new HashMap<>();
+          surveyMap.put("Navegador1", 700);
+          surveyMap.put("Navegador2", 500);
+          surveyMap.put("Navegador3", 400);
+          // surveyMap.put("Navegador4", 600);
+          // surveyMap.put("Navegador5", 300);
+          // surveyMap.put("Navegador6", 100);
+          modelAndView.addObject("surveyMap", surveyMap);
+
         return modelAndView;
     }
 
